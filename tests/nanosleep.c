151c97e134c92275f3d3d117efcbc16c7ad0b01f
Baojun Wang <wangbj@gmail.com>
2019-02-13 23:11:08 -0500

    Update nanosleep.c

--- tests/nanosleep.c
+++ tests/nanosleep.c
@@ -5,22 +5,50 @@
 #include <errno.h>
 #include <string.h>
 
+#define NITERATIONS 1000
+
+static long long diff_time(const struct timespec* begin,
+			   const struct timespec* end) {
+  long long r = 0;
+
+  r = end->tv_nsec - begin->tv_nsec;
+  if (end->tv_nsec < begin->tv_nsec) {
+    r += 1000000000;
+    r += 1000000000 * (end->tv_sec - begin->tv_sec - 1);
+  } else {
+    r += 1000000000 * (end->tv_sec - begin->tv_sec);
+  }
+
+  return r / 1000;
+}
+
 int main(int argc, char* argv[])
 {
   struct timespec req = {
     .tv_sec = 0,
     .tv_nsec = 100000000,
   };
-  struct timespec rem;
-  int ret;
+  struct timespec begin, end;
+  int ntests = NITERATIONS;
   
+  // ignore first nanosleep
+  nanosleep(&req, NULL);
+
+  // ignore first clock_gettime
+  clock_gettime(CLOCK_REALTIME, &end);
+
+  clock_gettime(CLOCK_REALTIME, &begin);
+
   for (int i = 0; i < 1000; i++) {
     printf("nanosleep, iteration: %u\n", i);
-    do {
-      ret = nanosleep(&req, &rem);
-      memcpy(&req, &rem, sizeof(req));
-    } while (ret != 0 && errno == EINTR);
+    nanosleep(&req, NULL);
   }
+  clock_gettime(CLOCK_REALTIME, &end);
+
+  long long elapsed = diff_time(&begin, &end);
+
+  fprintf(stderr, "time elapsed %lluus for %u iterations, mean: %.3lfus\n",
+	 elapsed, ntests, (double)elapsed / ntests);
 
   return 0;
 }
