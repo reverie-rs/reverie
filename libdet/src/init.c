f1826bd3d177f7c47a31a09ad2f186bc3ca5b924
Ryan Newton <rrnewton@gmail.com>
2019-02-17 10:31:21 -0500

    Minor: comments and a readme

--- libdet/src/init.c
+++ libdet/src/init.c
@@ -1,6 +1,11 @@
+
+// Provided by this systrace tool (.so):
 extern long captured_syscall(int, long, long, long, long, long, long);
+
+// Provided by the underlying instrumenation library:
 extern long untraced_syscall(int, long, long, long, long, long, long);
 
+// An initialization function for this systrace tool (.so):
 __attribute__((constructor)) static void __libdet_early_init(void) {
 	unsigned long* hook_addr = (unsigned long*)0x70001028UL;
 	*hook_addr = (unsigned long)&captured_syscall;
