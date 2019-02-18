4c9aa6287f86b1d20002d9f8c21f19cc14ee5358
Baojun Wang <wangbj@gmail.com>
2019-02-18 10:46:30 -0500

    change sysnr -> sysnum

--- build.rs
+++ build.rs
@@ -1,19 +1,19 @@
 use std::fs::File;
 use std::io::{Result, Write};
 use std::path::Path;
-use sysnr::gen_syscalls;
+use sysnum::gen_syscalls;
 
 use cc;
 
 fn gen_syscall_nrs() -> Result<()> {
     let dest_path = Path::new("src").join("nr.rs");
     let mut f = File::create(&dest_path)?;
-    writeln!(
-        f,
-        "#![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]\n"
-    )?;
     writeln!(f, "pub use self::SyscallNo::*;")?;
 
+    writeln!(
+        f,
+        "#[allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]\n"
+    )?;
     writeln!(f, "#[derive(Debug, PartialEq, Eq, Clone, Copy)]")?;
     writeln!(f, "pub enum SyscallNo {{")?;
     let syscalls = gen_syscalls().unwrap();
@@ -27,7 +27,7 @@ fn gen_syscall_nrs() -> Result<()> {
     }
     writeln!(f, "}}")?;
 
-    writeln!(f, "static syscall_names: [&str; {}] = [", syscalls.len())?;
+    writeln!(f, "static SYSCALL_NAMES: [&str; {}] = [", syscalls.len())?;
     for (name, _) in &syscalls {
         writeln!(
             f,
@@ -41,12 +41,12 @@ fn gen_syscall_nrs() -> Result<()> {
     writeln!(f, "    fn to_string(&self) -> String {{")?;
     writeln!(
         f,
-        "        syscall_names[self.clone() as usize].to_string()"
+        "        SYSCALL_NAMES[self.clone() as usize].to_string()"
     )?;
     writeln!(f, "    }}")?;
     writeln!(f, "}}")?;
 
-    writeln!(f, "static syscall_ids: [SyscallNo; {}] = [", syscalls.len())?;
+    writeln!(f, "static SYSCALL_IDS: [SyscallNo; {}] = [", syscalls.len())?;
     for (name, _) in &syscalls {
         writeln!(f, "    SYS{},", name.chars().skip(4).collect::<String>())?;
     }
@@ -54,10 +54,10 @@ fn gen_syscall_nrs() -> Result<()> {
 
     writeln!(f, "impl From<i32> for SyscallNo {{")?;
     writeln!(f, "    fn from(item: i32) -> Self {{")?;
-    writeln!(f, "        if item as usize > syscall_ids.len() {{")?;
+    writeln!(f, "        if item as usize > SYSCALL_IDS.len() {{")?;
     writeln!(f, "            panic!(\"invalid syscall: {{}}\", item)")?;
     writeln!(f, "        }} else {{")?;
-    writeln!(f, "            syscall_ids[item as usize].clone()")?;
+    writeln!(f, "            SYSCALL_IDS[item as usize]")?;
     writeln!(f, "        }}")?;
     writeln!(f, "    }}")?;
     writeln!(f, "}}")?;
