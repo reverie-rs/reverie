cf3834d153db7d232c029b091a15024ed0aa5814
Baojun Wang <wangbj@gmail.com>
2019-03-11 23:43:19 -0400

    Add build.rs for syscalls local crate

---
+++ syscalls/build.rs
@@ -0,0 +1,67 @@
+use std::fs::File;
+use std::io::{Result, Write};
+use std::path::PathBuf;
+use sysnum::gen_syscalls;
+
+fn gen_syscall_nrs(dest: PathBuf) -> Result<()> {
+    let mut f = File::create(dest)?;
+    writeln!(f, "pub use self::SyscallNo::*;")?;
+
+    writeln!(
+        f,
+        "#[allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]\n"
+    )?;
+    writeln!(f, "#[derive(Debug, PartialEq, Eq, Clone, Copy)]")?;
+    writeln!(f, "pub enum SyscallNo {{")?;
+    let syscalls = gen_syscalls().unwrap();
+    for (name, nr) in &syscalls {
+        writeln!(
+            f,
+            "    SYS{} = {},",
+            name.chars().skip(4).collect::<String>(),
+            nr
+        )?;
+    }
+    writeln!(f, "}}")?;
+
+    writeln!(f, "static SYSCALL_NAMES: [&str; {}] = [", syscalls.len())?;
+    for (name, _) in &syscalls {
+        writeln!(
+            f,
+            "    \"{}\",",
+            name.chars().skip(5).collect::<String>().as_str()
+        )?;
+    }
+    writeln!(f, "];")?;
+
+    writeln!(f, "impl ToString for SyscallNo {{")?;
+    writeln!(f, "    fn to_string(&self) -> String {{")?;
+    writeln!(
+        f,
+        "        SYSCALL_NAMES[self.clone() as usize].to_string()"
+    )?;
+    writeln!(f, "    }}")?;
+    writeln!(f, "}}")?;
+
+    writeln!(f, "static SYSCALL_IDS: [SyscallNo; {}] = [", syscalls.len())?;
+    for (name, _) in &syscalls {
+        writeln!(f, "    SYS{},", name.chars().skip(4).collect::<String>())?;
+    }
+    writeln!(f, "];")?;
+
+    writeln!(f, "impl From<i32> for SyscallNo {{")?;
+    writeln!(f, "    fn from(item: i32) -> Self {{")?;
+    writeln!(f, "        if item as usize > SYSCALL_IDS.len() {{")?;
+    writeln!(f, "            panic!(\"invalid syscall: {{}}\", item)")?;
+    writeln!(f, "        }} else {{")?;
+    writeln!(f, "            SYSCALL_IDS[item as usize]")?;
+    writeln!(f, "        }}")?;
+    writeln!(f, "    }}")?;
+    writeln!(f, "}}")?;
+
+    Ok(())
+}
+
+fn main() {
+    gen_syscall_nrs(PathBuf::from("src").join("nr.rs")).unwrap();
+}
