f1d86b2f9031c67f792cfb9e600082febce73e55
Baojun Wang <wangbj@gmail.com>
2019-05-02 16:52:01 -0400

    fix command line passing

--- benchmark/getpid-many-threaded.c
+++ benchmark/getpid-many-threaded.c
@@ -83,7 +83,7 @@ int main(int argc, char* argv[])
 
   if (argc == 3) {
     nthreads = atoi(argv[1]);
-    ntests = atoi(argv[1]);
+    ntests = atoi(argv[2]);
   }
 
   pthread_t *threads = alloca((1+nthreads) * sizeof(pthread_t));
