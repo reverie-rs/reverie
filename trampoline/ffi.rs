d87c7dee10febeee4b5fabdb14d5b82468bb6c0b
Baojun Wang <wangbj@gmail.com>
2019-06-10 16:14:46 -0400

    revert `staticlib` due to issue #48

--- trampoline/ffi.rs
+++ trampoline/ffi.rs
@@ -1,5 +1,5 @@
-//! ffi.rs: re-exports trampoline symbols.
-//!
+/// ffi.rs: re-exports trampoline symbols.
+///
 /// NB: rust (as of today's nightly) doesn't export symbols from .c/.S files,
 /// also rust doesn't seem to have visibility controls such as
 /// __attribute__((visibility("hidden"))), there's no good way to workaround
@@ -26,8 +26,73 @@ extern "C" {
                     syscall_insn: *mut c_void,
                     sp1: i64,
                     sp2: i64) -> i64;
+    fn _syscall_hook_trampoline();
+    fn _syscall_hook_trampoline_48_3d_01_f0_ff_ff();
+    fn _syscall_hook_trampoline_48_3d_00_f0_ff_ff();
+    fn _syscall_hook_trampoline_48_8b_3c_24();
+    fn _syscall_hook_trampoline_5a_5e_c3();
+    fn _syscall_hook_trampoline_89_c2_f7_da();
+    fn _syscall_hook_trampoline_90_90_90();
+    fn _syscall_hook_trampoline_ba_01_00_00_00();
+    fn _syscall_hook_trampoline_89_c1_31_d2();
+    fn _syscall_hook_trampoline_c3_nop();
+    fn _syscall_hook_trampoline_85_c0_0f_94_c2();
     fn _remote_syscall_helper();
     fn _remote_funccall_helper();
+}
+
+#[no_mangle]
+unsafe extern "C" fn syscall_hook_trampoline() {
+    _syscall_hook_trampoline()
+}
+
+#[no_mangle]
+unsafe extern "C" fn syscall_hook_trampoline_48_3d_01_f0_ff_ff() {
+    _syscall_hook_trampoline_48_3d_01_f0_ff_ff()
+}
+
+#[no_mangle]
+unsafe extern "C" fn syscall_hook_trampoline_48_3d_00_f0_ff_ff() {
+    _syscall_hook_trampoline_48_3d_00_f0_ff_ff()
+}
+#[no_mangle]
+unsafe extern "C" fn syscall_hook_trampoline_48_8b_3c_24() {
+    _syscall_hook_trampoline_48_8b_3c_24()
+}
+
+#[no_mangle]
+unsafe extern "C" fn syscall_hook_trampoline_5a_5e_c3() {
+    _syscall_hook_trampoline_5a_5e_c3()
+}
+
+#[no_mangle]
+unsafe extern "C" fn syscall_hook_trampoline_89_c2_f7_da() {
+    _syscall_hook_trampoline_89_c2_f7_da()
+}
+
+#[no_mangle]
+unsafe extern "C" fn syscall_hook_trampoline_90_90_90() {
+    _syscall_hook_trampoline_90_90_90()
+}
+
+#[no_mangle]
+unsafe extern "C" fn syscall_hook_trampoline_ba_01_00_00_00() {
+    _syscall_hook_trampoline_ba_01_00_00_00()
+}
+
+#[no_mangle]
+unsafe extern "C" fn syscall_hook_trampoline_89_c1_31_d2() {
+    _syscall_hook_trampoline_89_c1_31_d2()
+}
+
+#[no_mangle]
+unsafe extern "C" fn syscall_hook_trampoline_c3_nop() {
+    _syscall_hook_trampoline_c3_nop()
+}
+
+#[no_mangle]
+unsafe extern "C" fn syscall_hook_trampoline_85_c0_0f_94_c2() {
+    _syscall_hook_trampoline_85_c0_0f_94_c2()
 }
 
 #[no_mangle]
@@ -54,6 +119,11 @@ unsafe extern "C" fn untraced_syscall(
     arg5: i64) -> i64 {
     _raw_syscall(syscallno, arg0, arg1, arg2, arg3, arg4, arg5,
                  SYSCALL_UNTRACED as *mut _, 0, 0)
+}
+
+#[no_mangle]
+unsafe extern "C" fn remote_syscall_helper_do_not_call_me() {
+    _remote_syscall_helper();
 }
 
 #[repr(C)]
