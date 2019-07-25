bdee82055e4d2dbf5f84cd420a24ac44d8137e56
Baojun Wang <wangbj@gmail.com>
2019-07-24 21:30:51 -0400

    EXPERIMENTAL: setup seccomp with sepcial flag CLOEXEC

--- preloader/src/lib.rs
+++ preloader/src/lib.rs
@@ -31,6 +31,43 @@ struct sock_fprog {
 fn preload_dl_ns() -> Result<()>{
     if let Ok(dso) = std::env::var(consts::REVERIE_TRACEE_PRELOAD) {
         let linkmap = relink::dl_open_ns(dso);
+
+/*
+           struct sock_filter filter[] = {
+               /* [0] Load architecture from 'seccomp_data' buffer into
+                      accumulator */
+               BPF_STMT(BPF_LD | BPF_W | BPF_ABS,
+                        (offsetof(struct seccomp_data, arch))),
+
+               /* [1] Jump forward 5 instructions if architecture does not
+                      match 't_arch' */
+               BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, t_arch, 0, 1),
+
+               /* [2] Destination of system call number mismatch: allow other
+                      system calls */
+               BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW),
+
+               /* [3] Destination of architecture mismatch: kill task */
+               BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_KILL),
+	   };
+*/
+        let filter = vec![ 0x400000020u64,
+                           0xc000003e01000015u64,
+                           0x7fff000000000006u64,
+                           0x6u64 ];
+        let prog = sock_fprog {
+            len: 4,
+            filter: filter.as_ptr() as *const sock_filter,
+        };
+        let ptr = &prog as *const sock_fprog;
+        let r = unsafe {
+            libc::syscall(SYS_seccomp as i64,
+                          1,
+                          0,
+                          ptr as i64,
+                          0, 0, 0)
+        };
+        assert_eq!(r, 0);
         let mut whitelist: Vec<_> = vec![(0x7000_0002, 0x7000_0002)];
         linkmap.iter().for_each(|lm| {
             lm.ranges.iter().for_each(|e| {
@@ -50,11 +87,21 @@ fn preload_dl_ns() -> Result<()>{
         let r = unsafe {
             libc::syscall(SYS_seccomp as i64,
                           1,
-                          0,
+                          1 << 4,
                           ptr as i64,
                           0, 0, 0)
         };
-        assert_eq!(r, 0);
+        if r == -1 {
+            eprintln!("\n\n\t### seccomp doesn't support SECCOMP_FILTER_FLAG_CLOEXEC, execve may not work ###\n\n");
+            let r = unsafe {
+                libc::syscall(SYS_seccomp as i64,
+                              1,
+                              0,
+                              ptr as i64,
+                              0, 0, 0)
+            };
+            assert_eq!(r, 0);
+        }
     }
     Ok(())
 }
